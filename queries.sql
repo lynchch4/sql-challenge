aea34551af3e794a9a5783a1568ffbba72df4249
lynchch4 <lynchch4@gmail.com>
2023-02-24 23:00:42 -0500

    updated queries

--- queries.sql
+++ queries.sql
@@ -41,3 +41,17 @@ inner join employees
 inner join department
 	on(department.dept_no = d.dept_no)
 where dept_name = 'Sales';
+
+select employees.emp_no, last_name, first_name, dept_name
+from dept_employee d
+inner join employees
+	on(employees.emp_no = d.emp_no)
+inner join department
+	on(department.dept_no = d.dept_no)
+where dept_name = 'Sales'
+or dept_name = 'Development';
+
+select last_name, count(last_name) as "Occurances"
+from employees
+group by last_name
+order by "Occurances" DESC;
